621e5103eebf83742f715cbbb42ef8b29d184613
xianyizan <1966940448@qq.com>
2019-12-30 22:36:18 +0800

    提交原始数据

---
+++ use_of_lib/function/head.h
@@ -0,0 +1,4 @@
+#ifndef HEAD_H
+#define HEAD_H
+int add(int a, int b);
+#endif/*HEAD_H*/
---
+++ use_of_lib/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "function/head.h"
+
+using namespace std;
+int main(int argc,char *argv[])
+{
+	cout<<add(3,5)<<endl;
+	return 0;
+}
